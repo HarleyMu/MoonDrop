810bfdcbe547e85f7e07b23f06726a7dbb05f607
HarleyMu <759015127@qq.com>
2023-07-19 10:27:44 +0800

    test

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-07-18 18:12:57
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-07-18 18:18:59
+ * @FilePath: \MoonDrop\main.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello world\n");
+	return 0;
+}
